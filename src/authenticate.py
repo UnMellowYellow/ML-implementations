d8eef3cfb8d79ef3872059b68331bb1e0f6542da
Aditya Agarwal <adityag2511@gmail.com>
2016-09-22 20:16:20 +0530

    authenticate : generating vectors

--- src/authenticate.py
+++ src/authenticate.py
@@ -25,28 +25,46 @@ def get_request_data(file):
 def get_data_list(aname,directory):
 
 	# Checking for all names in the data directory
-	files 					= glob.glob(DATA_DIR+'/'+aname+'*.csv')
+	files 					= glob.glob(DATA_DIR+'/'+aname+'.csv')
 	if len(files) == 0:
-		print('No such user exists')
 		exit(2)
-	else:
-		print(files)
+
+	return files[0]
 
 
-def get_data(list):
+# Accessing the keystroke data of the specified user
+def get_data(file):
 
 	records 				= []
-
 	# Accessing each file to be retrieved
-	for file in list:
-		raw_data			= file.read()
+	with open(file,'r') as infile:
+		raw_data			= infile.read()
+
+	# Seperating each entry
+	records 				= raw_data.split('\n')
+	return records
 
 
+def format_data(data):
+
+	# Creating strucutes for various datasets
+	formatted_data 			= []
+	size 					= len(data[0].split(','))
+	for i in range(size):
+		formatted_data.append([])
+	for entry in data:
+		items 				= entry.split(',')
+		for i in range(size):
+			formatted_data[i].append(items[i])
+
+	print(formatted_data)
+	return formatted_data
 
 # Constants and predefined variables
 FILE_PATH 					= "../data/test.json"
 DATA_DIR					= "../output"
 
 request_data 				= get_request_data(FILE_PATH)
-user_files					= get_data_list(request_data['uname'],DATA_DIR)
-data 						= get_data(files)
+user_file					= get_data_list(request_data['uname'].lower(),DATA_DIR)
+raw_data 					= get_data(user_file)
+formatted_data 				= format_data(raw_data)
